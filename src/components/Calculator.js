975c87e44339f2b8c5912c38769fa200f2abbf84
Sanja <sanja1mandic@gmail.com>
2022-06-27 23:16:08 +0200

    make react componet for Keys in calculator, add event handleres them, and render it in calculator body

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,64 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Calculator.css';
+import PropTypes from 'prop-types';
+import calculate from '../logic/calculate';
 
 const raw1 = ['AC', '+/-', '%', '÷'];
-const raw2 = ['7', '8', '9', 'X'];
+const raw2 = ['7', '8', '9', 'x'];
 const raw3 = ['4', '5', '6', '-'];
 const raw4 = ['1', '2', '3', '+'];
 const raw5 = ['0', '.', '='];
 
-const raw1List = [];
-raw1.forEach((item) => {
-  raw1List.push(<button type="button" className="col-3">{item}</button>);
-});
-
-const raw2List = [];
-raw2.forEach((item) => {
-  raw2List.push(<button type="button" className="col-3">{item}</button>);
-});
-
-const raw3List = [];
-raw3.forEach((item) => {
-  raw3List.push(<button type="button" className="col-3">{item}</button>);
-});
-
-const raw4List = [];
-raw4.forEach((item) => {
-  raw4List.push(<button type="button" className="col-3">{item}</button>);
-});
-
-const raw5List = [];
-let count = 0;
-raw5.forEach((item) => {
-  let cl = 'col-3';
-  if (count === 0) {
-    cl = 'col-6';
+class Key extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
   }
-  raw5List.push(<button type="button" className={cl}>{item}</button>);
-  count += 1;
-});
+
+  handleClick() {
+    const { handleClick, value } = this.props;
+    handleClick(value);
+  }
+
+  render() {
+    const { className, value } = this.props;
+    return (
+      <button type="button" className={className} onClick={this.handleClick}>
+        {value}
+      </button>
+    );
+  }
+}
+
+Key.propTypes = {
+  className: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
+};
+
+function KeysRaw(props) {
+  let count = 0;
+  let cl = 'col-3';
+  const rawList = [];
+  props.raw.forEach((item) => {
+    if (count === 0 && props.raw[0] === '0') {
+      cl = 'col-6';
+    } else {
+      cl = 'col-3';
+    }
+    rawList.push(<Key className={cl} value={item} handleClick={props.handleClick} />);
+    count += 1;
+  });
+  return rawList;
+}
 
 function Calculator() {
+  const [obj, setObj] = useState({ total: '0', next: null, operation: null });
+  const { total, next, operation } = obj;
+
+  const handleClick = (value) => {
+    setObj(calculate(obj, value));
+  };
+
   return (
     <div className="Calculator-box">
       <div className="Display d-flex align-items-center justify-content-end p-2 h2 m-0">
-        0
+        {total}
+        {operation}
+        {next}
       </div>
       <div className="Calculator-body">
         <div className="raw">
-          { raw1List }
+          <KeysRaw raw={raw1} handleClick={handleClick} />
         </div>
         <div className="raw">
-          { raw2List }
+          <KeysRaw raw={raw2} handleClick={handleClick} />
         </div>
         <div className="raw">
-          { raw3List }
+          <KeysRaw raw={raw3} handleClick={handleClick} />
         </div>
         <div className="raw">
-          { raw4List }
+          <KeysRaw raw={raw4} handleClick={handleClick} />
         </div>
         <div className="raw">
-          { raw5List }
+          <KeysRaw raw={raw5} handleClick={handleClick} />
         </div>
       </div>
     </div>
