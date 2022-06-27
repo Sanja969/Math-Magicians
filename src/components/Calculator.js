776aed2b380d8cd9f07b08812dea6d3c879692d7
Sanja <sanja1mandic@gmail.com>
2022-06-27 17:14:06 +0200

    add babellrc file and update eslint file

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,65 +1,68 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './Calculator.css';
 
-let raw1 = ['AC', '+/-', '%', '÷'];
-let raw2 = ['7', '8', '9', 'X'];
-let raw3 = ['4', '5', '6', '-'];
-let raw4 = ['1', '2', '3', '+'];
-let raw5 = ['0', '.', '='];
+const raw1 = ['AC', '+/-', '%', '÷'];
+const raw2 = ['7', '8', '9', 'X'];
+const raw3 = ['4', '5', '6', '-'];
+const raw4 = ['1', '2', '3', '+'];
+const raw5 = ['0', '.', '='];
 
-let raw1List =  raw1.map((item, index) => {
-  return <button className="col-3" key = {index}>{item}</button>;
-})
+const raw1List = [];
+raw1.forEach((item) => {
+  raw1List.push(<button type="button" className="col-3">{item}</button>);
+});
 
-let raw2List =  raw2.map((item, index) => {
-  return <button className="col-3" key = {index}>{item}</button>;
-})
+const raw2List = [];
+raw2.forEach((item) => {
+  raw2List.push(<button type="button" className="col-3">{item}</button>);
+});
 
-let raw3List =  raw3.map((item, index) => {
-  return <button className="col-3" key = {index}>{item}</button>;
-})
+const raw3List = [];
+raw3.forEach((item) => {
+  raw3List.push(<button type="button" className="col-3">{item}</button>);
+});
 
-let raw4List =  raw4.map((item, index) => {
-  return <button className="col-3" key = {index}>{item}</button>;
-})
+const raw4List = [];
+raw4.forEach((item) => {
+  raw4List.push(<button type="button" className="col-3">{item}</button>);
+});
 
-let raw5List =  raw5.map((item, index) => {
-  let cl = 'col-3'
-  if(index===0) {
+const raw5List = [];
+let count = 0;
+raw5.forEach((item) => {
+  let cl = 'col-3';
+  if (count === 0) {
     cl = 'col-6';
   }
-  return <button className={ cl } key = {index}>{item}</button>;
-})
+  raw5List.push(<button type="button" className={cl}>{item}</button>);
+  count += 1;
+});
 
-const Calculator = () => {
-
+function Calculator() {
   return (
-      <div className='Calculator-box'>
-        <div className='Display d-flex align-items-center justify-content-end p-2 h2 m-0'>
-          0
+    <div className="Calculator-box">
+      <div className="Display d-flex align-items-center justify-content-end p-2 h2 m-0">
+        0
+      </div>
+      <div className="Calculator-body">
+        <div className="raw">
+          { raw1List }
         </div>
-        <div className='Calculator-body'>
-          <div className='raw'>
-            { raw1List }
-          </div>
-          <div className='raw'>
-            { raw2List }
-          </div>
-          <div className='raw'>
-            { raw3List }
-          </div>
-          <div className='raw'>
-            { raw4List }
-          </div>
-          <div className='raw'>
-            { raw5List }
-          </div>
+        <div className="raw">
+          { raw2List }
+        </div>
+        <div className="raw">
+          { raw3List }
+        </div>
+        <div className="raw">
+          { raw4List }
+        </div>
+        <div className="raw">
+          { raw5List }
         </div>
       </div>
-  )
+    </div>
+  );
 }
-
-
 
 export default Calculator;
