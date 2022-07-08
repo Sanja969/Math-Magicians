1b4cd7d3acd8eb28c8b61f755a0be52c105b2953
Sanja <sanja1mandic@gmail.com>
2022-07-08 11:37:23 +0200

    import bootstap js in index,js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/js/bootstrap';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
