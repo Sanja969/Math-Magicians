10e1a085402f4b8d794e8b7ece705761e5330b60
Sanja <sanja1mandic@gmail.com>
2022-07-08 11:36:13 +0200

    import router in app module and all components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Quote from './pages/Quote';
+import Header from './components/Header';
+import Home from './pages/Home';
+import NoMatch from './pages/NoMatch';
+import './App.css';
 
 function App() {
   return (
-    <Calculator />
+    <>
+      <Router>
+        <Header className="header" />
+
+        <Routes>
+          <Route
+            path="/calculator"
+            element={(
+              <div className="w-100 d-flex flex-column align-items-center math-container">
+                <h2>Let&lsquo;s do some math!</h2>
+                <Calculator />
+              </div>
+            )}
+          />
+          <Route path="/quotes" element={<Quote />} />
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
